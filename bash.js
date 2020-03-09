4402fb78348acd776829432cd48a8f126fbc0c21
rodrigojm <rodrigomallmann1@gmail.com>
2020-03-09 12:40:59 -0400

    ls done

--- bash.js
+++ bash.js
@@ -1,11 +1,18 @@
 process.stdout.write('prompt > ');
+
 const global = {
-    pwd:  require('./pwdTESTTEST/pwdTEST/pwd.js')
+    pwd:  require('./pwd.js'),
+    ls: require('./ls.js')
 }
 
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
+    
     if (global[cmd] !== undefined) {
+        if (typeof global[cmd] === 'function') {
+            global[cmd]();
+        } 
+
         process.stdout.write(global[cmd]);
     } else {
         process.stdout.write('You typed: ' + cmd);
---
+++ ls.js
@@ -0,0 +1,17 @@
+var fs = require("fs");
+
+const ls = () => {
+    fs.readdir('./', 'utf8', (err, files) => {
+        if (err) {
+            throw err
+        } else {
+            process.stdout.write(files.join('\n'))
+            process.stdout.write("prompt > ")
+        }
+    })
+}
+
+
+
+module.exports = ls;
+
