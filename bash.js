de2f7d3b4087525084a7d9072e328bcad8a8f324
rodrigojm <rodrigomallmann1@gmail.com>
2020-03-09 11:50:48 -0400

    added pwd

--- bash.js
+++ bash.js
@@ -1,8 +1,17 @@
 process.stdout.write('prompt > ');
+const global = {
+    pwd: process.env.PWD
+}
 
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
-
-    process.stdout.write('You typed: ' + cmd);
+    if (global[cmd] !== undefined) {
+        process.stdout.write(global[cmd]);
+    } else {
+        process.stdout.write('You typed: ' + cmd);
+    }
+    
     process.stdout.write('\nprompt > ');
 })
+
+
